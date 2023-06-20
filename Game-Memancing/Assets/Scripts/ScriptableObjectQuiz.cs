70df007e64fc08236b1c4bf2324ecc16935dc883
19Hamid91 <99725605+19Hamid91@users.noreply.github.com>
2023-06-20 22:32:11 +0700

    add text karakteristik dan mengubah tampilan koleksi

---
+++ Game-Memancing/Assets/Scripts/ScriptableObjectQuiz.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QuizScriptableObject", menuName = "Create Scriptable Object")]
+public class ScriptableObjectQuiz : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
