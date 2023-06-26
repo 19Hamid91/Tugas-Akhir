b31df37a5d106a9d2afb7f7f9ad92aa2dc600505
19Hamid91 <99725605+19Hamid91@users.noreply.github.com>
2023-06-26 11:01:04 +0700

    memperbaiki koleksi dan deskripsi ikan

--- Game-Memancing/Assets/Scripts/UIManager.cs
+++ Game-Memancing/Assets/Scripts/UIManager.cs
@@ -17,28 +17,28 @@ public class UIManager : MonoBehaviour
     {
         for (int i = 0; i < cardManager.cards.Count; i++)
         {
-            if(Data.unlockedFish.Contains(i))
-            {
+            // if(Data.unlockedFish.Contains(i))
+            // {
                 //Assign nama ikan
-                cardSlots[i].transform.GetChild(2).GetComponent<Text>().text = cardManager.cards[i].nama;
+                cardSlots[i].transform.GetChild(1).GetComponent<Text>().text = cardManager.cards[i].nama;
 
                 //Assign gambar ikan
-                cardSlots[i].transform.GetChild(3).GetComponent<Image>().sprite = cardManager.cards[i].cardSprite;
+                cardSlots[i].transform.GetChild(2).GetComponent<Image>().sprite = cardManager.cards[i].cardSprite;
 
                 //Activate button
-                cardSlots[i].transform.GetChild(4).gameObject.SetActive(true);
-            }
-            else
-            {
-                //Assign nama ikan
-                cardSlots[i].transform.GetChild(2).GetComponent<Text>().text = "?????";
+                cardSlots[i].transform.GetChild(3).gameObject.SetActive(true);
+            // }
+            // else
+            // {
+            //     //Assign nama ikan
+            //     cardSlots[i].transform.GetChild(1).GetComponent<Text>().text = "?????";
 
-                //Assign gambar ikan
-                cardSlots[i].transform.GetChild(3).GetComponent<Image>().sprite = cardManager.cards[i].cardSiluetSprite;
+            //     //Assign gambar ikan
+            //     cardSlots[i].transform.GetChild(2).GetComponent<Image>().sprite = cardManager.cards[i].cardSiluetSprite;
 
-                //Deactivate button
-                cardSlots[i].transform.GetChild(4).gameObject.SetActive(false);
-            }
+            //     //Deactivate button
+            //     cardSlots[i].transform.GetChild(3).gameObject.SetActive(false);
+            // }
         }
     }
 }
