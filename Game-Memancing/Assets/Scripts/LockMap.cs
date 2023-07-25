fba643a0c472ba0f33c91f15ce41696495f40c8e
19Hamid91 <99725605+19Hamid91@users.noreply.github.com>
2023-07-25 22:28:43 +0700

    update map (membuat area terkunci)

--- Game-Memancing/Assets/Scripts/LockMap.cs
+++ Game-Memancing/Assets/Scripts/LockMap.cs
@@ -6,30 +6,39 @@ using UnityEngine.UI;
 public class LockMap : MonoBehaviour
 {
     public GameObject[] mapObject;
+    public GameObject[] pin;
+    public Sprite[] mapLocked;
+    public GameObject bgMap;
     private int unlockedMap;
     // Start is called before the first frame update
     void Start()
     {
         unlockedMap = 1;
+        bgMap.GetComponent<Image>().sprite = mapLocked[0];
         if (Data.PapuaLevel == 4)
         {
             unlockedMap += 5;
+            bgMap.GetComponent<Image>().sprite = mapLocked[4];
         }
         else if (Data.SulawesiLevel == 4)
         {
             unlockedMap += 4;
+            bgMap.GetComponent<Image>().sprite = mapLocked[4];
         }
         else if (Data.SumatraLevel == 4)
         {
             unlockedMap += 3;
+            bgMap.GetComponent<Image>().sprite = mapLocked[3];
         }
         else if (Data.KalimantanLevel == 4)
         {
             unlockedMap += 2;
+            bgMap.GetComponent<Image>().sprite = mapLocked[2];
         }
         else if (Data.JawaLevel == 4)
         {
             unlockedMap += 1;
+            bgMap.GetComponent<Image>().sprite = mapLocked[1];
         }
         lockMap();
     }
@@ -40,6 +49,9 @@ public class LockMap : MonoBehaviour
         {
             // lock map
             mapObject[i].transform.GetChild(2).gameObject.SetActive(false);
+
+            // hide pin
+            pin[i].SetActive(false);
         }
     }
 
